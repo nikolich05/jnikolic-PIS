1d2a6503840ee680265899c332218d94ffc9a79f
amrki <amrkon01@fesb.hr>
2026-03-23 20:14:06 +0100

    nova metoda

--- Class1.cs
+++ Class1.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace jnikolic
 {
+
     internal class Class1
     {
+        public void metoda()
+        {
+
+        }
     }
+
+
 }
